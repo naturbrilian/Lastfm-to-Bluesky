fdcebc340823d5be3a1fb4419617260a5dd338a8
Lieba Natur Brilian <gingalibadeidara@gmail.com>
2025-09-15 14:42:15 +0700

    delete song links because length character on bluesky

--- with_image.py
+++ with_image.py
@@ -3,19 +3,19 @@ import requests
 from atproto import Client, models
 
 # 🔑 Konfigurasi
-LASTFM_USER = "Username"
-LASTFM_API_KEY = "Your_Apikey"
+LASTFM_USER = "Your_Username"
+LASTFM_API_KEY = "Your_LastFM_Apikey"
 
 BLUESKY_HANDLE = "Your_Handle_Bsky"
-BLUESKY_PASSWORD = "Your_App_Password"
+BLUESKY_PASSWORD = "Your_bsky_password"
 
 # 🎵 Ambil data Now Playing dari Last.fm
 def get_now_playing(user, api_key):
     url = "http://ws.audioscrobbler.com/2.0/"
     params = {
         "method": "user.getrecenttracks",
-        "user": "Username",
-        "api_key": "Your_Apikey",
+        "user": "Your_Username",
+        "api_key": "Your_LastFM_Apikey",
         "format": "json",
         "limit": 1
     }
@@ -25,23 +25,22 @@ def get_now_playing(user, api_key):
 
 def main():
     client = Client()
-    client.login("Your_Handle_Bsky", "Your_App_Password")
+    client.login("Your_Handle_Bsky", "Your_Bsky_Password")
 
     last_posted = None  # buat cek biar gak dobel
 
     while True:
         try:
-            track = get_now_playing('Your_LastFM_Username', 'Your_Lastfm_Api_Key')
+            track = get_now_playing('Your_Lastfm_Username', 'Your_ApikeY')
 
             artist = track["artist"]["#text"]
             title = track["name"]
             album = track["album"]["#text"]
             cover_url = track["image"][-1]["#text"]
-            url = track["url"]
 
             unique_id = f"{artist}-{title}"  # biar gak dobel post
             if unique_id != last_posted:
-                message = f"🎶 Now Playing:\nArtist: {artist}\nTitle: {title}\nAlbum: {album}\n{url}"
+                message = f"Recently Played:\nArtist: {artist}\nTitle: {title}\nAlbum: {album}"
 
                 img_data = requests.get(cover_url).content
                 upload = client.com.atproto.repo.upload_blob(img_data)
@@ -54,7 +53,7 @@ def main():
                 )
 
                 client.send_post(text=message, embed=embed)
-                print("✅ Post sukses:", message)
+                print("Post sukses:", message)
 
                 last_posted = unique_id
 
